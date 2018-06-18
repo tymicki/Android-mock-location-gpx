8fafa137f83a90e38405e619e080883f8015802a
Jacek Tymicki <jacek.tymicki@mobica.com>
2018-06-18 18:00:28 +0200

    mock location changes via commandline

--- app/src/main/java/com/mockloc/jtymicki/androidmocklocation/MainActivity.kt
+++ app/src/main/java/com/mockloc/jtymicki/androidmocklocation/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.mockloc.jtymicki.androidmocklocation
 
+import android.app.AppOpsManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*;
 
 
 /**
@@ -28,5 +36,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        enableMockLocation.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        handleMockLocationAccess()
+    }
+
+    private fun handleMockLocationAccess() {
+        if (isMockSettingsON()) {
+            mockLocationPermission.setText(R.string.mock_location_granted)
+            mockLocationPermission.setTextColor(Color.GREEN)
+            enableMockLocation.visibility = View.GONE
+        } else {
+            mockLocationPermission.setText(R.string.mock_location_not_granted)
+            mockLocationPermission.setTextColor(Color.RED)
+            enableMockLocation.visibility = View.VISIBLE
+        }
+    }
+
+    private fun isMockSettingsON(): Boolean {
+        var isMockLocation = false
+        isMockLocation = try {
+            //if marshmallow
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val opsManager = this.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+                opsManager.checkOp(AppOpsManager.OPSTR_MOCK_LOCATION,
+                        android.os.Process.myUid(),
+                        BuildConfig.APPLICATION_ID) == AppOpsManager.MODE_ALLOWED
+            } else {
+                // in marshmallow this will always return true
+                android.provider.Settings.Secure.getString(this.contentResolver, "mock_location") != "0"
+            }
+        } catch (e: Exception) {
+            return isMockLocation
+        }
+        return isMockLocation
+
     }
 }
---
+++ app/src/main/java/com/mockloc/jtymicki/androidmocklocation/MockGPSReceiver.kt
@@ -0,0 +1,39 @@
+package com.mockloc.jtymicki.androidmocklocation
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.util.Log
+
+class MockGPSReceiver : BroadcastReceiver() {
+    private val TAG = "MockGPSReceiver"
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(TAG, "on receive invoked")
+        var mockGPS: MockLocationProvider? = null
+        var mockWifi: MockLocationProvider? = null
+        if (intent.getAction() == "stop.mock") {
+            if (mockGPS != null) {
+                mockGPS.shutdown()
+            }
+            if (mockWifi != null) {
+                mockWifi.shutdown()
+            }
+        } else {
+            mockGPS = MockLocationProvider(LocationManager.GPS_PROVIDER, context)
+            mockWifi = MockLocationProvider(LocationManager.NETWORK_PROVIDER, context)
+            val lat: Double
+            val lon: Double
+            val alt: Double
+            val accuracy: Float
+            lat = java.lang.Double.parseDouble(if (intent.getStringExtra("lat") != null) intent.getStringExtra("lat") else "0")
+            lon = java.lang.Double.parseDouble(if (intent.getStringExtra("lon") != null) intent.getStringExtra("lon") else "0")
+            alt = java.lang.Double.parseDouble(if (intent.getStringExtra("alt") != null) intent.getStringExtra("alt") else "0")
+            accuracy = java.lang.Float.parseFloat(if (intent.getStringExtra("accuracy") != null) intent.getStringExtra("accuracy") else "0")
+            Log.i(TAG, String.format("setting mock to Latitude=%f, Longitude=%f Altitude=%f Accuracy=%f", lat, lon, alt, accuracy))
+            mockGPS.pushLocation(lat, lon, alt, accuracy)
+            mockWifi.pushLocation(lat, lon, alt, accuracy)
+        }
+    }
+}
